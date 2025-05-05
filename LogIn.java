5164e263a8249380a9c3e12f8c73140fda10ec2a
More-Pe <peraltamorena.va@gmail.com>
2025-05-05 12:58:27 +0200

    feature: add Log In solution

--- LogIn.java
+++ LogIn.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class LogIn {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, si son iguales a los datos dados imprimirá en terminal: Acceso concedido, si no son iguales imprimirá en terminal: Nombre de usuario o contraseña incorrecta.
         //Averigua como hacer para poder ingresar el dato justo al lado de lo que termine la pregunta, es decir, que no haga un salto de línea.
-
+        String usuario = "MorePe";
+        String clave = "12345";
+        Scanner login = new Scanner(System.in);
+        System.out.println("Ingresa tu nombre de usuario: ");
+        String user = login.nextLine();
+        System.out.println("Ingresa tu contraseña ");
+        String password = login.nextLine();
+        if (user.equals(usuario) && password.equals(clave)) {
+            System.out.println("Acceso concedido");
+        } else {
+            System.out.println("Nombre de usuario o contraseña incorrecta");
+        }
+        login.close();
     }
 }
